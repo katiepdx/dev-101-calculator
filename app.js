9f50440c4f3837933ac413f853de2798bbf1bc7f
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-02-11 12:47:33 -0800

    add calculator

--- app.js
+++ app.js
@@ -1,5 +1,43 @@
-// import functions and grab DOM elements
+// ADDITION
+const add1 = document.getElementById('add-1')
+const add2 = document.getElementById('add-2')
+const addTotal = document.getElementById('add-total')
+const addButton = document.getElementById('add-button')
 
-// initialize state
+addButton.addEventListener('click', () => {
+  const total = Number(add1.value) + Number(add2.value)
+  addTotal.textContent = total
+})
 
-// set event listeners to update state and DOM
+// SUBTRACT
+const subtract1 = document.getElementById('subtract-1')
+const subtract2 = document.getElementById('subtract-2')
+const subtractTotal = document.getElementById('subtract-total')
+const subtractButton = document.getElementById('subtract-button')
+
+subtractButton.addEventListener('click', () => {
+  const total = Number(subtract1.value) - Number(subtract2.value)
+  subtractTotal.textContent = total
+})
+
+// MULTIPLICATION
+const multiplication1 = document.getElementById('multiply-1')
+const multiplication2 = document.getElementById('multiply-2')
+const multiplicationTotal = document.getElementById('multiply-total')
+const multiplicationButton = document.getElementById('multiply-button')
+
+multiplicationButton.addEventListener('click', () => {
+  const total = Number(multiplication1.value) * Number(multiplication2.value)
+  multiplicationTotal.textContent = total
+})
+
+// DIVIDE
+const divide1 = document.getElementById('divide-1')
+const divide2 = document.getElementById('divide-2')
+const divideTotal = document.getElementById('divide-total')
+const divideButton = document.getElementById('divide-button')
+
+divideButton.addEventListener('click', () => {
+  const total = Number(divide1.value) / Number(divide2.value)
+  divideTotal.textContent = total
+})
