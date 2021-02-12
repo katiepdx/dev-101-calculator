63e5e092a4b85c1a09c3492e12e1abde6d44841f
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-02-11 21:17:51 -0800

    fix spacing

--- calculatorUtils.js
+++ calculatorUtils.js
@@ -4,15 +4,15 @@ export const multiply = ((num1, num2) => num1 * num2);
 export const divide = ((num1, num2) => num1 / num2);
 
 export const calculator = ((firstInputId, secondInputId, buttonId, resultsId, calculation) => {
-  const input1 = document.getElementById(firstInputId);
-  const input2 = document.getElementById(secondInputId);
-  const display = document.getElementById(resultsId);
-  const button = document.getElementById(buttonId);
+    const input1 = document.getElementById(firstInputId);
+    const input2 = document.getElementById(secondInputId);
+    const display = document.getElementById(resultsId);
+    const button = document.getElementById(buttonId);
 
-  button.addEventListener('click', () => {
-    const num1 = Number(input1.value);
-    const num2 = Number(input2.value);
-    const total = calculation(num1, num2);
-    display.textContent = total;
-  });
+    button.addEventListener('click', () => {
+        const num1 = Number(input1.value);
+        const num2 = Number(input2.value);
+        const total = calculation(num1, num2);
+        display.textContent = total;
+    });
 });
